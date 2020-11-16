73aef8136ac9dcd45f33219615f3594fb129a771
Almaz Idiyatshin <idiyatshinalmaz@gmail.com>
2020-11-16 22:37:29 +0300

    Update Step 5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -15,35 +15,36 @@ const askName = () => {
 
 const name = askName();
 
-const askQuestions = (count) => {
-  if (count < 3) {
-    const randomNum = getRandomNum();
-    const answer = readlineSync.question(`Question: ${randomNum} `);
-    console.log(`Your answer: ${answer}`);
-  
-    if (answer !== 'yes' && answer !== 'no') {
-      console.log(`'${answer}' is wrong answer ;(. Let's try again, ${name}!"`);
-      return;
+const askQuestions = () => {
+  const iter = (count) => {
+    if (count < 3) {
+      const randomNum = getRandomNum();
+      const answer = readlineSync.question(`Question: ${randomNum} `);
+      console.log(`Your answer: ${answer}`);
+      if (answer !== 'yes' && answer !== 'no') {
+        console.log(`'${answer}' is wrong answer ;(. Let's try again, ${name}!"`);
+      }
+      if (answer === 'yes' && randomNum % 2 === 0) {
+        console.log('Correct!');
+        iter(count + 1);
+      }
+      if (answer === 'yes' && randomNum % 2 !== 0) {
+        console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!"`);
+      }
+      if (answer === 'no' && randomNum % 2 !== 0) {
+        console.log('Correct!');
+        iter(count + 1);
+      }
+      if (answer === 'no' && randomNum % 2 === 0) {
+        console.log(`'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${name}!"`);
+      }
     }
-    if (answer === 'yes' && randomNum % 2 === 0) {
-      console.log('Correct!');
-      return askQuestions(count + 1);
+    if (count >= 3) {
+      console.log(`Congratulations, ${name}!`);
     }
-    if (answer === 'yes' && randomNum % 2 !== 0) {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!"`);
-      return;
-    }
-    if (answer === 'no' && randomNum % 2 !== 0) {
-      console.log('Correct!');
-      return askQuestions(count + 1);
-    }
-    if (answer === 'no' && randomNum % 2 === 0) {
-      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${name}!"`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
+  };
+  return iter(0);
 };
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-askQuestions(0);
+askQuestions();
